ee46b79eb93317a40fb9ac03bec143e20b9e1761
amilin <moh.amilin@moladin.com>
2023-02-22 19:32:11 +0700

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default {
+  displayName: 'rest-api-news',
+  preset: './jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/rest-api-news',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+};
